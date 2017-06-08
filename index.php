9bcdefe62054b04a779220869d90c25eaaf60505
Cushbin <shamon@cushbin.com>
2017-06-08 10:21:50 +0530

    Added refeal function

--- index.php
+++ index.php
@@ -5,12 +5,17 @@ include 'share.php';
 $share = new Share();
 
 /*
- * calculate the amount of each parent you should call this after user registration
- * params $userId
- * $amount
+ * calculate the amount of each parent you should call this after adding referee
+ * params $userId userid of the user
+ * $amount Amount to calculated
  */
 $share->calculateShare(2,100);
 
 /*
- * Add referals
+ * Add referals for a particular user
+ * Params Array [$parentId,$userId]
+ * $parentId  userId of user who is refering someone else
+ * $userId   $userId of person being refered
  */
+
+$share->addReferals([1,5]);
